445387335f7b81431b1ae71ad587169d06f86cb4
maenginner <mauricio.giraldo@mail.escuelaing.edu.co>
2015-11-18 17:54:12 -0500

    first commit

--- src/main/resources/static/js/libs/appmodule.js
+++ src/main/resources/static/js/libs/appmodule.js
@@ -1,8 +1,55 @@
-(function () {
+
     var app = angular.module('modone', []);
-})();
+    app.controller("BlueprintController",function ($scope, $http){
+		$scope.blunames=[];
+		$scope.blupoints=[];
+		$scope.bname="";		
+		
+		$scope.loadData = function() {
+			
+  		  	var configList = {
+        	method: "GET",
+        	url: "blueprints"
+    	};
+		
+		
+    		var response=$http(configList);
 
-
-
-
-
+    		response.success(function(data, status, headers, config) {
+				$scope.blunames = data;
+	    	});
+	   		response.error(function(data, status, headers, config) {
+	        	alert("The petition has failed. HTTP Status:"+status);
+    		});
+		};
+		
+		
+		$scope.loadShape = function (){
+			var configList = {
+				method: "GET",
+				url: "blueprints/"+$scope.bname	
+			};
+			
+			var response = $http(configList);
+			var cnv = document.getElementById("drawing");
+			var ctx = cnv.getContext("2d");
+			
+			response.success (function(data,status,headers,config){
+				alert("EEXITO");
+				$scope.blupoints=data.points;
+                                //for (var i=0; i<$scope.blupoints.length;i++){
+                                alert("blueprints/"+$scope.bname);
+                                    ctx.moveTo($scope.blupoints[0].getX(),$scope.blupoints[0].getY());
+                                    ctx.lineTo($scope.blupoints[1].getX(),$scope.blupoints[1].getY());
+                                            
+                                //}
+				
+			});
+			
+			response.error(function(data,status,headers,config){
+				alert("FAAAIL");
+			});
+			
+		};
+	
+});
